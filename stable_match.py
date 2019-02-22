b0a34efa34c403cb94b68817a60e2e8ede4b6bb4
gagandeepsinghbrar <gaganbrarca@gmail.com>
2019-02-21 22:06:33 -0800

    Well there you go finally did it

--- stable_match.py
+++ stable_match.py
@@ -1,12 +1,12 @@
 
 import itertools as it
-
+import math
 class CountStableMatches():
 
 	def __init__(self,filename):
 
 		self.filename=filename
-		self.count=0
+		
 
 	def readFile(self):
 
@@ -24,8 +24,8 @@ class CountStableMatches():
 			# got men preferences and women preferences
 			self.men_preferences=[list(map(lambda num: int(num),inputFile.readline().split(" "))) for i in range(self.n)]
 			self.women_preferences=[list(map(lambda num: int(num),inputFile.readline().split(" "))) for i in range(self.n)]
-
-
+		
+			self.count=math.factorial(self.n)
 
 	def countTotal(self):
 
@@ -42,13 +42,17 @@ class CountStableMatches():
 
 		for b in range(len(self.allCombinations)):
 
+			TokeepChecking=True
 
 			for n in range(len(self.allCombinations[b])):
 
 
+				if not TokeepChecking:
+					break
 				# allCombinations[b] is tuple ( 3 4 1 2 )
 
 				# allCombinations[b][0] is 3 the women
+				
 
 
 				# preference list for man currently [ 4 3 1 2 ]
@@ -58,13 +62,14 @@ class CountStableMatches():
 				# current women 
 
 				women=self.allCombinations[b][n]
+
+				
 				
 				# her index in man preference 
 				
 				women_index=man_list.index(women)
 
-
-
+				
 
 				
 
@@ -72,10 +77,12 @@ class CountStableMatches():
 				if man_list[:women_index]:
 
 
+					
+
 					# moving backwards
 					for _ in range(women_index-1,-1,-1):
 
-
+						
 
 
 						
@@ -87,34 +94,25 @@ class CountStableMatches():
 
 						women_list=self.women_preferences[w_index]
 
+						
+						
 
 						w_suspect_index_in_wp=women_list.index(n+1)
 
 
+						# Quick and easy testing trick instead of looking at a lot of data
 
-						if women_list[:w_suspect_index_in_wp]:
-							
+						# if self.allCombinations[b]==(3,1,2,4) and n==0 and _==1:
+						# 	print(w_suspect_index_in_wp)
 
-
-							print(man_list)
-
-
-
-
-							if n+1 in women_list[:w_suspect_index_in_wp]:
-
-								print("cheated")
-						else:
+						if w_suspect_index_in_wp<women_index:
+							self.count=self.count-1;
+							TokeepChecking=False;
 							break
-
-
-
-
-
-
-				
-
-			print("------")
+						
+						
+		print(self.count)
+		return self.count
 
 
 
@@ -138,35 +136,6 @@ if __name__=="__main__":
 	prob=CountStableMatches("input1.txt")
 	prob.readFile()
 	prob.countTotal()
-
-
-
-
-
-
-
-
-
-
-# for batchNo in range(len(allCombinations)):
-	
-# 	# looping over that particular combination
-
-# 	for n in range(len(allCombinations[batchNo])):
-
-
-
-		
-		
-# 		i_of_women=men_preferences[n].index(allCombinations[batchNo][n])
-# 		# n[:i] gives cheaters on the left
-# 		if men_preferences[n][:i_of_women]:
-
-# 			for jump in range(i_of_women,-1,-1):
-
-# 				# men_preferences[n][jump]-1   -> index of suspected women
-# 				#allCombinations[batchNo].index(men_preferences[n][jump])+1 -> who is she matched with
-
 
 
 				
